de6e38a83e9c99e7f93ba6ae848c904f5eea80cf
Jannifer Ramirez <jennycr@Jannifers-MacBook-Pro.local>
2025-06-17 11:47:53 -0700

    added lab2.py

---
+++ lab2.py
@@ -0,0 +1,31 @@
+# Lab 2: Python Profiles
+
+## Step 1: Name
+name = "Jannifer Calmo Ramirez"
+
+## Step 2: Nickname
+nickname = "Jenny"
+
+## Step 3: Age
+age = 17
+
+## Step 4: Programming Language
+used_python = False
+
+## Step 5
+# a LIST to store your hobbies
+hobbies = ["rhythm+", "badminton", "reading", "watching anime", "hanging out with friends" ]
+
+## Step 6
+# a DICTIONARY to store your favorite things
+favorite_things = {"movie": "substance", "food": "ceviche", "animal": "leopard", "color": "light or royale green", "sport": "badminton and volleyball"}
+
+## Optional: add your own variable here!
+
+school_twins = ["Natalie", "Lana", "Zi", "Allison", "Tarryn", "Anthony"]
+
+# set variable
+#should have no dupes when printed
+teachers = {"Mr. Robeson", "Mr. Hernandez", "Ms. Blide", "Mr. Acuna", "Mr. Robeson"}
+print(teachers)
+
